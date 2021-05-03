d1266256c85e948e71424a113ea464884b3d96f9
Ekeu Ulrich <ekeuulrich2@gmail.com>
2021-05-03 12:36:22 +0200

    Testing requests methods CORS

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,7 +2,7 @@ import cookie from 'cookie';
 import { API_URL } from '@/config/index';
 
 export default async (req, res) => {
-  if (req.method === 'POST' || req.method === 'OPTIONS') {
+  if (req.method === 'POST') {
     const { username, email, password, photoURL } = req.body;
 
     const strapiFetch = await fetch(`${API_URL}/auth/local/register`, {
@@ -39,7 +39,7 @@ export default async (req, res) => {
         .json({ message: data.message[0].messages[0].message });
     }
   } else {
-    res.setHeader('Allow', ['POST', 'OPTIONS']);
+    res.setHeader('Allow', ['POST']);
     res.status(405).json({ message: `Method ${req.method} not allowed` });
   }
 };
